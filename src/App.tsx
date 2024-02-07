d6535ff9728cadec660cc2fc2ee0963b4937b050
manudevz <enmanuelmarcanoservices@gmail.com>
2024-02-07 20:00:51 -0300

    x

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline text-blue-600 bg-slate-300 dark:bg-black dark:text-white">
+        Hello world!
+      </h1>
+    </>
+  )
+}
+
+export default App
