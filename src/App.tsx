9ffbac7ffa1b5759d1f77df43c2f269db85acfeb
manudevz <enmanuelmarcanoservices@gmail.com>
2024-02-09 22:14:39 -0300

    x

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <>
       <h1 className="text-3xl font-bold underline text-blue-600 bg-slate-300 dark:bg-black dark:text-white">
-        Hello world!
+        Hello 
       </h1>
     </>
   )
